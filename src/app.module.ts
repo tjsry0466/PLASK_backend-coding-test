18016776d8e9b02c3250cb8a4bffc448506adbc1
jeong seonkyo <seonkyo0466@gmail.com>
2021-12-20 23:37:36 +0900

    generate Users, Shops, Products CRUD Resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ShopsModule } from './shops/shops.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ShopsModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
